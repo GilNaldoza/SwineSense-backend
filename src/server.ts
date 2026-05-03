4f150ca6358d4efa6dbe3fd10eacbe2af52d1341
GilNaldoza <naldoza.gil30@gmail.com>
2026-05-03 20:40:51 +0800

    update server

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import { startGrpcServer } from './grpc';
 import { startRestServer } from './rest';
 
-const GRPC_PORT = process.env.GRPC_PORT || '50051';
+const GRPC_PORT = process.env.GRPC_PORT || '50052';
 const REST_PORT = process.env.PORT || '3000';
 
 console.log("Starting LENS Backend V2...");
