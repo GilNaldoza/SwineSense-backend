3dcdb5451078e1605e315e32b93a9e5ed37cfec7
GilNaldoza <naldoza.gil30@gmail.com>
2026-05-03 13:54:30 +0800

    update .env.local

--- src/rest.ts
+++ src/rest.ts
@@ -12,7 +12,10 @@ import analyticsRoutes from './api/analytics';
 export const startRestServer = (port: string) => {
     const app = express();
 
-    app.use(cors());
+    app.use(cors({
+        origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5000'],
+        credentials: true
+    }));
     app.use(express.json());
     app.use(morgan('dev'));
 
